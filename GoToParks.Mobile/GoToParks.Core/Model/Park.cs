b75ee86ce9bef35722b71f89a23423e76784a887
Adrian Limon <me@adrianlimon.com>
2016-03-25 21:51:35 -0700

    added park model

---
+++ GoToParks.Mobile/GoToParks.Core/Model/Park.cs
@@ -0,0 +1,11 @@
+﻿namespace GoToParks.Core.Model
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Long { get; set; }
+    }
+}
