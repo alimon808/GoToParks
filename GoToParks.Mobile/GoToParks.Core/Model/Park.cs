bae2bc4591d447ee5e6beee49a3dd4bccbe19576
Adrian Limon <me@adrianlimon.com>
2016-03-29 22:26:20 -0700

    added hours property

--- GoToParks.Mobile/GoToParks.Core/Model/Park.cs
+++ GoToParks.Mobile/GoToParks.Core/Model/Park.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string Hours { get; set; }
         public double Lat { get; set; }
         public double Long { get; set; }
     }
