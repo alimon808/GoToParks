06f81f5ff0ad3be8ea4f591af8e44c5b4499a992
Adrian Limon <me@adrianlimon.com>
2016-04-06 16:20:15 -0700

    added park base fragment

---
+++ GoToParks.Mobile/GoToParks.Droid/Fragments/BaseFragment.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using GoToParks.Core;
+using GoToParks.Core.Service;
+
+namespace GoToParks.Droid.Fragments
+{
+    public class BaseFragment : Fragment
+    {
+        protected ListView listView;
+        protected ParkDataService dataService;
+        protected List<Park> parks;
+
+        public BaseFragment()
+        {
+            dataService = new ParkDataService();
+        }
+
+        protected void HandleEvents()
+        {
+            listView.ItemClick += ListView_ItemClick;
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var park = parks[e.Position];
+            var intent = new Intent();
+            intent.SetClass(this.Activity, typeof(ParkDetailActivity));
+            intent.PutExtra("selectedParkId", park.Id);
+            StartActivityForResult(intent, 100);
+        }
+    }
+}
